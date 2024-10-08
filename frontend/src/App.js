8d26a1f9886b51a85340a8cb2199cb9ef337fc97
rachitpoudel101 <rachit300poudel@gmail.com>
2024-10-08 11:27:25 +0545

    I have learned about how to handle Event and how to set state in react app.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './component/Navbar';
+import Textform from './component/Textform';
 
 function App() {
   return (
-    <Navbar title="Navbar Icon" about="About Me"/>
+    <>
+    <Navbar title="Navbar Icon" about="About Me" />
+    <div className="container my-3">
+      <Textform heading ='Enter the text to analysis'/>
+    </div>
+    </>
   );
 }
+
 
 export default App;
---
+++ frontend/src/component/Textform.js
@@ -0,0 +1,22 @@
+import React,{ useState } from 'react'
+export default function Textform(props) {
+  const handleUpclick =()=>{
+    console.log("Uppercase was clicked")
+    setText("You have clicked on the handleUpclick")
+  }
+
+  const handleOnChnage =(event)=>{
+    console.log("On Chnage");
+    setText(event.target.value);
+  }
+  const [text, setText] =useState('Enter text Here'); 
+  return (
+    <div>
+        <div classNmae="mb-3">
+            <h1>{props.heading}</h1>
+            <textarea className="form-control" value={text} onChange={handleOnChnage} id="mybox" rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary my-3" onClick={handleUpclick}>Change to Uppercase</button>
+    </div>
+  )
+}
