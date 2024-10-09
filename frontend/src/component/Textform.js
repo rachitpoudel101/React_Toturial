d02d356bb4aa3aae57c7c8e819ad345c90af43df
rachitpoudel101 <rachit300poudel@gmail.com>
2024-10-09 08:54:13 +0545

    add lower case button and word count character count and the time counter.

--- frontend/src/component/Textform.js
+++ frontend/src/component/Textform.js
@@ -6,18 +6,35 @@ export default function Textform(props) {
     setText(newText)
   }
 
+  const handlelowclick =()=>{
+    // console.log(text)
+    let newText = text.toLocaleLowerCase();
+    setText(newText)
+  }
+
   const handleOnChnage =(event)=>{
     // console.log("On Chnage");
     setText(event.target.value);
   }
   const [text, setText] =useState('Enter text Here'); 
   return (
-    <div>
+    <>
+    <div className='container'>
         <div classNmae="mb-3">
             <h1>{props.heading}</h1>
             <textarea className="form-control" value={text} onChange={handleOnChnage} id="mybox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary my-3" onClick={handleUpclick}>Change to Uppercase</button>
+        <br/>
+        <button className="btn btn-primary my-3 mr-3" onClick={handlelowclick}>Change to Lowcase</button>
     </div>
+    <div className='container my-2'>
+    <h1> Yours text summary </h1>
+    <p> {text.split(" ").length} words and {text.length} character</p> 
+    <p> {0.008 *text.split(" ").length} Minutes</p> 
+    <h4> Preview</h4>
+    <p>{text}</p>
+    </div>
+    </>
   )
 }
