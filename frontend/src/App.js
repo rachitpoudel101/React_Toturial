05d09bc4ecd212f10efc7b7d99fc4849594bfe4d
rachitpoudel101 <rachit300poudel@gmail.com>
2024-10-11 21:12:12 +0545

    I have Improve the Dark mode and Light mode.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 import About from './component/About';
 import Navbar from './component/Navbar';
-// import Textform from './component/Textform';
+import Textform from './component/Textform';
 
 function App() {
+  const [mode, setMode] =useState('light');
+  const toggleMode= ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-    <Navbar title="Navbar Icon" about="About Me" />
+    <Navbar title="Navbar Icon" about="About Me" mode={mode} toggleMode={toggleMode} />
     <div className="container my-3">
-      {/* <Textform heading ='Enter the text to analysis'/> */}
+      <Textform heading ='Enter the text to analysis'/>
       <About/>
     </div>
     </>
