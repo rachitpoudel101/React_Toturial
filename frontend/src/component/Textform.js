50208e41a717fc451334b74b94b56b7c2a7794ab
rachitpoudel101 <rachit300poudel@gmail.com>
2024-10-13 21:48:57 +0545

    I have Improve the Dark mode and Light mode.

--- frontend/src/component/Textform.js
+++ frontend/src/component/Textform.js
@@ -22,16 +22,16 @@ export default function Textform(props) {
   const [text, setText] =useState('Enter text Here'); 
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{color: props.mode === 'dark'? 'white':"black"}}>
         <div classNmae="mb-3">
             <h1>{props.heading}</h1>
-            <textarea className="form-control" value={text} onChange={handleOnChnage} style={{backgroundColor: props.mode === 'light'? 'grey':"white"}} id="mybox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChnage} style={{backgroundColor: props.mode === 'light'? 'grey':'white',color :props.mode === 'dark'? 'white':"black"}} id="mybox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary my-3" onClick={handleUpclick}>Change to Uppercase</button>
         <button className="btn btn-primary my-3 mx-3" onClick={handlelowclick}>Change to Lowcase</button>
         <button className="btn btn-primary my-3 mx-3" onClick={cleartext}>Clear Text</button>
     </div>
-    <div className='container my-2'>
+    <div className='container my-2' style={{color: props.mode === 'dark'? 'white':"black"}}>
     <h1> Yours text summary </h1>
     <p> {text.split(" ").length} words and {text.length} character</p> 
     <p> {0.008 *text.split(" ").length} Minutes</p> 
