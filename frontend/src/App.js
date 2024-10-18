41eae5de2b7c384ecaacdd412ede1dfae79a88ea
rachitpoudel101 <rachit300poudel@gmail.com>
2024-10-18 20:12:43 +0545

    additional notifiaction for the alert in dark and light mode.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useSyncExternalStore } from 'react';
 import './App.css';
 import About from './component/About';
 import Alert from './component/Alert';
@@ -7,20 +7,30 @@ import Textform from './component/Textform';
 
 function App() {
   const [mode, setMode] =useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message,type)=>{
+      setAlert ({
+        msg : message,
+        type: type
+      })
+  }
   const toggleMode= ()=>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = 'grey';
+      showAlert("Dark mode has been enabled", "success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("light mode has been enabled", "success");
     }
   }
   return (
     <>
     <Navbar title="Navbar Icon" about="About Me" mode={mode} toggleMode={toggleMode} />
-    <Alert alert="alertfor now"/>
+    <Alert alert={alert}/>
     <div className="container my-3">
       <Textform heading ='Enter the text to analysis' mode={mode}/>
       <About/>
